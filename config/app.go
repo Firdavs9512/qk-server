839de8c7d5c7707c55ab8054b4ad18a404e0f84c
Firdavs <firdavs20fe@gmail.com>
2024-03-21 22:17:21 +0300

    [change] Update AppType add MaxFileSize variable

--- config/app.go
+++ config/app.go
@@ -1,21 +1,23 @@
 package config
 
 type AppType struct {
-	Name      string
-	Version   string
-	Author    string
-	UploadUrl string
-	AppHost   string
-	AppPort   int
+	Name        string
+	Version     string
+	Author      string
+	UploadUrl   string
+	AppHost     string
+	AppPort     int
+	MaxFileSize int64
 }
 
 var (
 	App = AppType{
-		Name:      "QK Server",
-		Version:   "0.0.1",
-		Author:    "Firdavs",
-		UploadUrl: "uploads/",
-		AppHost:   "localhost",
-		AppPort:   2595,
+		Name:        "QK Server",
+		Version:     "0.0.1",
+		Author:      "Firdavs",
+		UploadUrl:   "uploads/",
+		AppHost:     "localhost",
+		AppPort:     2595,
+		MaxFileSize: 10 << 20, // 10MB
 	}
 )
