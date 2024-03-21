4277bc4046e7f991b71cb74ad95b20d38bcac5a8
Firdavs <firdavs20fe@gmail.com>
2024-03-21 21:46:30 +0300

    [add] config file add new upload url

--- config/app.go
+++ config/app.go
@@ -1,15 +1,17 @@
 package config
 
 type AppType struct {
-	Name    string
-	Version string
-	Author  string
+	Name      string
+	Version   string
+	Author    string
+	UploadUrl string
 }
 
 var (
 	App = AppType{
-		Name:    "QK Server",
-		Version: "0.0.1",
-		Author:  "Firdavs",
+		Name:      "QK Server",
+		Version:   "0.0.1",
+		Author:    "Firdavs",
+		UploadUrl: "uploads/",
 	}
 )
